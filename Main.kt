df3b0807914064c71eb82c8c8e3fb0a4d3cc40e6
JimmyGab121 <110641288+JimmyGab121@users.noreply.github.com>
2023-01-02 23:08:36 -0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    print("A continuación se van a mostrar los tipos de vehiculos que existen")
+    println()
+    val readOnly: List<String> = listOf("Carros", "Motocicletas", "Barcos", "Aviones")
+    print("El tamaño de la cadena es:")
+    println()
+    print(readOnly.size)
+    println()
+    print("Veremos el dato que ocupa la posición que indiquemos aquí")
+    println()
+    print(readOnly.get(3))
+    println()
+    print("Este es el contenido de la lista")
+    println()
+    print(readOnly)
+}
+
+
+
+
